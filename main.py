800175f447d0ba4dfe270a9c2b496bf836249401
Monadios <gfalkenthros@gmail.com>
2015-11-10 19:54:17 +0100

    More info in listing feature

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import sys
 import praw
+import warnings
+
+
+warnings.filterwarnings("ignore")
 
 r = praw.Reddit(user_agent='reddit-cli')
 sub = r.get_subreddit('linux')
 comments = []
 
-def get_subreddit_posts(subreddit, post_limit):
+def get_subreddit_posts(subreddit, post_limit=None):
+    if post_limit == None:
+        post_limit = 10
+        
     posts = []
     for post in subreddit.get_hot(limit=post_limit):
         posts.append(post)
@@ -17,3 +25,10 @@ def list_posts(posts):
         print(words[1] + ' :: ' + words[0])
         print(post.url)
         print('-' * length)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: reddit-cli URL")
+        exit()
+    list_posts(get_subreddit_posts(r.get_subreddit(sys.argv[1])))
